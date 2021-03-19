cfe8ca86ba29ebb7ed0b884d32a03db752c04c93
Dan <denise97220@gmail.com>
2021-03-19 21:28:49 +0800

    add CRUD

--- app.js
+++ app.js
@@ -3,18 +3,11 @@
 const express = require("express")
 const app = express()
 
-// 載入 mongoose
+// 載入 Rest model
+const Rest = require("./models/rest")
+
+// 載入及設定 mongoose
 const mongoose = require("mongoose")
-
-// 載入 handlebars 及 JSON 檔案
-const exphbs = require("express-handlebars")
-const restList = require("./restaurant.json")
-
-
-// 定義要使用的連接埠號 (port number) 
-const port = 3000
-
-// 設定連線到 mongoDB
 mongoose.connect("mongodb://localhost/rest-list", { useNewUrlParser: true, 
 useUnifiedTopology: true })
 
@@ -26,6 +19,18 @@ db.on("error", () => {
 db.once("open", () => {
   console.log("mongodb connected!")
 })
+
+// 載入 handlebars 及 JSON 檔案
+const exphbs = require("express-handlebars")
+const restList = require("./restaurant.json")
+
+// 載入及設定 body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+// 定義要使用的連接埠號 (port number) 
+const port = 3000
 
 // 將 template engine 設定為 handlerbars
 // 第一個參數是樣板引擎的名稱，第二個參數是樣板引擎相關的設定
@@ -43,16 +48,69 @@ app.use(express.static("public"))
 
 // 設定 index 頁面路由
 app.get("/", (req,res) => {
-    res.render("index", { restaurants: restList.results })
+  Rest.find()
+      .lean()
+      .then(rests => res.render("index", { restaurants: rests }))
+      .catch(error => console.log(error))
 })
 
 
 // 設定 show 頁面路由
 app.get("/restaurants/:id", (req,res) => {
-    const filterRest = restList.results.filter( rest => rest.id.toString() === req.params.id)
-    res.render("show", { rest: filterRest[0] } )
+  const id = req.params.id
+  Rest.findById(id)
+      .lean()
+      .then(rest => res.render("show", { rest } ))
+      .catch(error => console.log(error))
 })
 
+// 設定新增餐廳頁面路由
+app.get("/new", (req,res) => {
+  return res.render("new")
+})
+
+app.post("/restaurants", (req,res) => {
+  const newRest = req.body
+  Rest.create(newRest)
+      .then(() => res.redirect("/"))
+      .catch(error => console.log(error))
+})
+
+// 設定 delete 路由
+app.post("/restaurants/:id/delete", (req,res) => {
+  const id = req.params.id
+  Rest.findById(id)
+      .then(rest => rest.remove())
+      .then(() => res.redirect("/"))
+      .catch(error => console.log(error))
+})
+
+// 設定 edit 路由
+app.get("/restaurants/:id/edit", (req,res) => {
+  const id = req.params.id
+  Rest.findById(id)
+      .lean()
+      .then(rest => res.render("edit", { rest }))
+      .catch(error => console.log(error))
+})
+
+app.post("/restaurants/:id/edit", (req,res) => {
+  const id = req.params.id
+  const editedRest = req.body
+  Rest.findById(id)
+      .then(rest => {
+        rest.name = editedRest.name
+        rest.category = editedRest.category
+        rest.phone = editedRest.phone
+        rest.rating = editedRest.rating
+        rest.location = editedRest.location
+        rest.description = editedRest.description
+        rest.image = editedRest.image
+        return rest.save()
+      })
+      .then(() => res.redirect("/"))
+      .catch(error => console.log(error))
+})
 
 // 設定 search 頁面路由
 app.get("/search", (req,res) => {
